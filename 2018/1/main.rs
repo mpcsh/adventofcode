f114e3a95668c1c85427d4613027745b04627061
Mark Cohen <git@mail.mpc.sh>
2018-12-01 12:12:48 -0600

    2018-1-2 done!

--- 2018/1/main.rs
+++ 2018/1/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::collections::HashSet;
-use std::collections::Vec;
+use std::vec::Vec;
 
 fn part_1(deltas: &Vec<i32>) -> i32 {
     let mut frequency: i32 = 0;
@@ -15,11 +15,17 @@ fn part_1(deltas: &Vec<i32>) -> i32 {
 
 fn part_2(deltas: &Vec<i32>) -> i32 {
     let mut frequency: i32 = 0;
-    let mut seen_frequencies = HashSet::new();
+    let mut seen_frequencies: HashSet<i32> = HashSet::new();
+    seen_frequencies.insert(0);
 
     loop {
         for delta in deltas {
-        }
+            frequency += delta;
+            if seen_frequencies.contains(&frequency) {
+                return frequency;
+            };
+            seen_frequencies.insert(frequency);
+        };
     }
 }
 
@@ -32,13 +38,12 @@ fn main() {
 
     let mut deltas: Vec<i32> = Vec::new();
     for line in contents.split("\n") {
-        let delta: i32 = match line.parse() {
-            Ok(i) => i,
-            Err(_) => 0
+        let _ = match line.parse() {
+            Ok(i) => deltas.push(i),
+            Err(_) => ()
         };
-        deltas.push(delta);
     };
 
-    println!("Part 1: frequency is {}", part_1(deltas));
-    println!("Part 2: first frequency reached twice is {}", part_2(deltas));
+    println!("Part 1: frequency is {}", part_1(&deltas));
+    println!("Part 2: first frequency reached twice is {}", part_2(&deltas));
 }
