74546c79ffb558a6fd8adea3f407bb10183b3b58
Mark Cohen <m@mpc.sh>
2018-12-01 12:00:45 -0600

    2018-1 pull out file parsing into main()

--- 2018/1/main.rs
+++ 2018/1/main.rs
@@ -1,17 +1,26 @@
 use std::fs;
+use std::collections::HashSet;
+use std::collections::Vec;
 
-fn part_1(contents: String) -> i32 {
+fn part_1(deltas: &Vec<i32>) -> i32 {
     let mut frequency: i32 = 0;
 
-    for line in contents.split("\n") {
-        let delta: i32 = match line.parse() {
-            Ok(i) => i,
-            Err(_) => 0
-        };
+    for delta in deltas {
         frequency += delta
     };
 
     frequency
+}
+
+
+fn part_2(deltas: &Vec<i32>) -> i32 {
+    let mut frequency: i32 = 0;
+    let mut seen_frequencies = HashSet::new();
+
+    loop {
+        for delta in deltas {
+        }
+    }
 }
 
 
@@ -21,5 +30,15 @@ fn main() {
         Err(_) => String::new()
     };
 
-    println!("Part 1: frequency is {}", part_1(contents));
+    let mut deltas: Vec<i32> = Vec::new();
+    for line in contents.split("\n") {
+        let delta: i32 = match line.parse() {
+            Ok(i) => i,
+            Err(_) => 0
+        };
+        deltas.push(delta);
+    };
+
+    println!("Part 1: frequency is {}", part_1(deltas));
+    println!("Part 2: first frequency reached twice is {}", part_2(deltas));
 }
