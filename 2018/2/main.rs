069701112565eed47e003241314d1838e1638712
Mark Cohen <git@mail.mpc.sh>
2018-12-02 15:01:14 -0600

    2018-2-2 done!

--- 2018/2/main.rs
+++ 2018/2/main.rs
@@ -28,7 +28,38 @@ fn part_1(tags: &Vec<String>) -> u16 {
     exactly_two * exactly_three
 }
 
+fn strcmp(s1: &String, s2: &String) -> u64 {
+    let mut diff = 0;
+    for (c1, c2) in s1.chars().zip(s2.chars()) {
+        if c1 != c2 {
+            diff += 1;
+        };
+    };
+
+    diff
+}
+
+fn common_letters(s1: &String, s2: &String) -> String {
+    let mut i = 0;
+    for (c1, c2) in s1.chars().zip(s2.chars()) {
+        if c1 != c2 {
+            return [&s1[..i], &s2[i + 1..]].join("");
+        };
+        i += 1
+    };
+
+    s1.clone()
+}
+
 fn part_2(tags: &Vec<String>) -> String {
+    for tag1 in tags {
+        for tag2 in tags {
+            if strcmp(tag1, tag2) == 1 {
+                return common_letters(tag1, tag2);
+            };
+        };
+    };
+
     String::from("")
 }
 
@@ -43,6 +74,7 @@ fn main() -> Result<(), std::io::Error> {
     };
 
     println!("Part 1: checksum = {}", part_1(&tags));
+    println!("Part 2: common tag letters = {}", part_2(&tags));
 
     Ok(())
 }
