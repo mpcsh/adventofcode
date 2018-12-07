4b05c128ba63c51beed0e86080f441eec4b948a9
Mark Cohen <m@mpc.sh>
2018-12-06 23:01:04 -0600

    2018-3-1 done!

--- 2018/3/main.rs
+++ 2018/3/main.rs
@@ -1,7 +1,9 @@
 use std::fs;
 use std::fmt;
 use std::vec::Vec;
+use std::collections::HashMap;
 
+#[derive(PartialEq, Eq, Hash)]
 struct Point {
     x: i64,
     y: i64
@@ -12,6 +14,9 @@ impl fmt::Display for Point {
         write!(f, "({}, {})", self.x, self.y)
     }
 }
+
+type ClaimId = i64;
+type Canvas = HashMap<Point, Vec<ClaimId>>;
 
 struct Claim {
     id: i64,
@@ -27,24 +32,53 @@ impl fmt::Display for Claim {
     }
 }
 
-fn claim_init(line: &String) -> Claim {
-    let fields: Vec<i64> = line
-        .split(|c| vec![' ', '#', '@', ',', ':', 'x'].contains(&c))
-        .filter(|f| *f != "")
-        .map(|f| f.parse::<i64>().unwrap())
-        .collect();
+impl Claim {
+    fn new(line: &String) -> Claim {
+        let fields: Vec<i64> = line
+            .split(|c| vec![' ', '#', '@', ',', ':', 'x'].contains(&c))
+            .filter(|f| *f != "")
+            .map(|f| f.parse::<i64>().unwrap())
+            .collect();
 
-    Claim {
-        id: fields[0],
-        origin: Point { x: fields[1], y: fields[2] },
-        width: fields[3],
-        height: fields[4]
+        Claim {
+            id: fields[0],
+            origin: Point { x: fields[1], y: fields[2] },
+            width: fields[3],
+            height: fields[4]
+        }
+    }
+
+    fn place_point(&self, canvas: &mut Canvas, point: Point) -> () {
+        canvas.entry(point).or_insert(Vec::new()).push(self.id)
+    }
+
+    fn place(&self, canvas: &mut Canvas) -> () {
+        let Point {x, y} = self.origin;
+        let (width, height) = (self.width, self.height);
+
+        for r in y..y + height {
+            for c in x..x + width {
+                self.place_point(canvas, Point {x: c, y: r});
+            };
+        };
     }
 }
 
 fn part_1(claims: &Vec<Claim>) -> i64 {
+    let mut canvas: Canvas = HashMap::new();
 
-    0
+    for claim in claims {
+        claim.place(&mut canvas);
+    };
+
+    let mut num_overlaps = 0;
+    for (_, claimed_by) in canvas.iter() {
+        if claimed_by.len() >= 2 {
+            num_overlaps += 1;
+        };
+    };
+
+    num_overlaps
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -57,8 +91,7 @@ fn main() -> Result<(), std::io::Error> {
         };
     };
 
-    let claims = lines.into_iter().map(|l| claim_init(&l)).collect();
-
+    let claims: Vec<Claim> = lines.into_iter().map(|l| Claim::new(&l)).collect();
     println!("Part 1: in^2 of fabric overlapped = {}", part_1(&claims));
 
     Ok(())
