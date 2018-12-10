8bfd7f349c5cb076ce990521dfda31e0f105191e
Mark Cohen <git@mail.mpc.sh>
2018-12-09 22:51:37 -0600

    2018-5-1 done!

--- 2018/5/main.rs
+++ 2018/5/main.rs
@@ -1,14 +1,34 @@
 use std::fs;
 
-fn main() -> Result<(), std::io::Error> {
-    let contents: String = fs::read_to_string("input.txt")?;
+fn flip_case(c: &char) -> char {
+    ((*c as u8) ^ 0x20) as char
+}
 
-    let mut lines: Vec<String> = Vec::new();
-    for line in contents.split("\n") {
-        if line != "" {
-            lines.push(line.to_string());
+fn part_1(polymer: &mut Vec<char>) -> u64 {
+    let mut i = 0;
+    loop {
+        if i == polymer.len() - 1 {
+            return polymer.len() as u64;
+        };
+
+        if polymer[i] == flip_case(&polymer[i + 1]) {
+            // removes the chars that start at i and i + 1
+            polymer.remove(i);
+            polymer.remove(i);
+            if i != 0 {
+                i -= 1;
+            };
+        } else {
+            i += 1;
         };
     };
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let polymer: String = fs::read_to_string("input.txt")?.trim().to_string();
+
+    println!("Part 1: reacted length = {}",
+             part_1(&mut polymer.chars().collect()));
 
     Ok(())
 }
