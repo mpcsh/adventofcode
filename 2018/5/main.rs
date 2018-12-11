1ed453ba6b63157e93271bd459d8cc75d8e46727
Mark Cohen <m@mpc.sh>
2018-12-10 21:18:10 -0600

    2018-5-2 done!

--- 2018/5/main.rs
+++ 2018/5/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn flip_case(c: &char) -> char {
     ((*c as u8) ^ 0x20) as char
@@ -24,11 +25,36 @@ fn part_1(polymer: &mut Vec<char>) -> u64 {
     };
 }
 
+fn part_2(polymer: &String) -> u64 {
+    let mut chars_seen: HashSet<char> = HashSet::new();
+    for c in polymer.chars() {
+        let _ = chars_seen.insert(c.to_ascii_lowercase());
+    };
+
+    let mut min = None;
+    for &c in chars_seen.iter() {
+        let cut_polymer: String = polymer
+            .split(|p| p == c || p == c.to_ascii_uppercase())
+            .flat_map(|s| s.chars())
+            .collect();
+
+        let len = part_1(&mut cut_polymer.chars().collect());
+        if min == None || len < min.unwrap() {
+            min = Some(len);
+        };
+    };
+
+    min.unwrap()
+}
+
 fn main() -> Result<(), std::io::Error> {
     let polymer: String = fs::read_to_string("input.txt")?.trim().to_string();
 
     println!("Part 1: reacted length = {}",
              part_1(&mut polymer.chars().collect()));
+
+    println!("Part 2: min length = {}",
+             part_2(&polymer));
 
     Ok(())
 }
