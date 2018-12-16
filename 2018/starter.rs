61a3c93ed55150fc581ec7f44b1068b1f043028b
Mark Cohen <m@mpc.sh>
2018-12-16 14:28:18 -0600

    Accept command-line input for input filename
    Co-authored-by: Sophie Welber <sjwelber@gmail.com>

--- 2018/starter.rs
+++ 2018/starter.rs
@@ -1,7 +1,9 @@
+use std::env;
 use std::fs;
 
 fn main() -> Result<(), std::io::Error> {
-    let contents: String = fs::read_to_string("input.txt")?;
+    let args: Vec<String> = env::args().collect();
+    let contents: String = fs::read_to_string(&args[1])?;
 
     let mut lines: Vec<String> = Vec::new();
     for line in contents.split("\n") {
