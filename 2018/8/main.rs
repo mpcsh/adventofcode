4308c7513ab2fef8eb354238c31d4a5c0e9eb082
Mark Cohen <git@mail.mpc.sh>
2018-12-19 08:45:45 -0600

    2018-8-1 done!
    Co-authored-by: Sophie Welber <sjwelber@gmail.com>

--- 2018/8/main.rs
+++ 2018/8/main.rs
@@ -1,16 +1,89 @@
 use std::env;
 use std::fs;
 
+
+struct Node {
+    num_children: u64,
+    num_metadata: u64,
+    children: Vec<Node>,
+    metadata: Vec<u64>
+}
+
+impl Node {
+    fn init(raw: &[u64]) -> Node {
+        let num_children = raw[0] as u64;
+        let num_metadata = raw[1] as u64;
+
+        Node {
+            num_children,
+            num_metadata,
+            children: vec![],
+            metadata: vec![]
+        }
+
+    }
+
+    fn from_raw_helper(raw: &[u64]) -> (Option<Node>, &[u64]) {
+        if raw.len() < 2 {
+            return (None, raw);
+        }
+
+        let mut node = Node::init(raw);
+        let mut remaining: &[u64] = &raw[2..];
+
+        for _ in 0..(node.num_children) {
+            let (maybe_child, new_remaining) = Node::from_raw_helper(remaining);
+            match maybe_child {
+                None => (),
+                Some(child) => node.children.push(child)
+            };
+            remaining = new_remaining;
+        };
+
+        for i in 0..(node.num_metadata as usize) {
+            node.metadata.push(remaining[i]);
+        };
+
+        remaining = &remaining[(node.num_metadata as usize)..];
+
+        (Some(node), remaining)
+    }
+
+    fn from_raw(raw: Vec<u64>) -> Option<Node> {
+        let (node, _) = Node::from_raw_helper(&raw[..]);
+        node
+    }
+}
+
+fn part_1(tree: &Node) -> u64 {
+    let mut count: u64 = tree.metadata
+        .iter()
+        .fold(0, |acc, x| acc + x);
+
+    count += tree.children
+        .iter()
+        .fold(0, |acc, child| acc + part_1(&child));
+
+    count
+}
+
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     let contents: String = fs::read_to_string(&args[1])?;
 
-    let mut lines: Vec<String> = Vec::new();
-    for line in contents.split("\n") {
-        if line != "" {
-            lines.push(line.to_string());
-        };
-    };
+    let raw: Vec<u64> = contents
+        .split_whitespace()
+        .map(|s| {
+            match s.parse::<u64>() {
+                Ok(i) => i,
+                Err(_) => panic!("Couldn't parse file")
+            }
+        })
+        .collect();
+
+    let tree = Node::from_raw(raw).unwrap();
+
+    println!("Part 1: sum of all metadata entries = {}", part_1(&tree));
 
     Ok(())
 }
