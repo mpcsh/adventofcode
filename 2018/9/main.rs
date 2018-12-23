0d42c5e6db8e9257530e5436f6c3987372d359dc
Mark Cohen <m@mpc.sh>
2018-12-22 18:22:10 -0600

    2018-9-1 got rotation working but counterclockwise test won't print
    Co-authored-by: Sophie Welber <sjwelber@gmail.com>

--- 2018/9/main.rs
+++ 2018/9/main.rs
@@ -1,33 +1,79 @@
 use std::env;
-use std::cell::RefCell;
-use std::rc::Rc;
-use std::borrow::BorrowMut;
+use std::collections::VecDeque;
 
-struct CirclePoint {
-    marble: u64,
-    clockwise: Rc<RefCell<CirclePoint>>,
-    counterclockwise: Rc<RefCell<CirclePoint>>
+struct Circle<T> {
+    current: Option<T>,
+    left: VecDeque<T>,
+    right: VecDeque<T>
 }
 
-fn insert_clockwise_of(current_marble: Rc<RefCell<CirclePoint>>, new_marble: u64) {
-    // set up new marble
-    let new_marble_point = Rc::new(RefCell::new(CirclePoint {
-        marble: new_marble,
-        clockwise: current_marble.borrow().clockwise.clone(),
-        counterclockwise: current_marble.clone()
-    }));
+impl<T: std::fmt::Debug> Circle<T> {
+    fn new() -> Self {
+        Circle {
+            current: None,
+            left: VecDeque::new(),
+            right: VecDeque::new()
+        }
+    }
 
-    // change adjacent references
-    current_marble.borrow().clockwise.borrow().borrow_mut().counterclockwise = new_marble_point.clone();
-    current_marble.borrow().clockwise = new_marble_point.clone();
-}
+    fn insert(self: &mut Circle<T>, elem: T) {
+        let _ = self.current
+            .take()
+            .map(|e| self.right.push_front(e));
+        self.current = Some(elem);
+    }
 
-fn place_marble(current_marble: Rc<RefCell<CirclePoint>>, new_marble: u64)
-                -> (u64, Rc<RefCell<CirclePoint>>) {
-    if new_marble % 23 == 0 {
-        (0, current_marble)
-    } else {
-        (0, current_marble)
+    fn rotate_clockwise(&mut self) {
+        // push onto left
+        self.current.take().map(|e| self.left.push_back(e));
+
+        // rotate the lists
+        self.left.pop_front().map(|e| self.right.push_back(e));
+
+        // take from right
+        self.right.pop_front().map(|e| self.current = Some(e));
+    }
+
+    fn rotate_counterclockwise(&mut self) {
+        // push onto right
+        self.current.take().map(|e| self.right.push_front(e));
+
+        // rotate the lists
+        self.right.pop_back().map(|e| self.left.push_front(e));
+
+        // take from left
+        self.left.pop_back().map(|e| self.current = Some(e));
+    }
+
+    // positive num_rot goes clockwise, negative num_rot goes counterclockwise
+    fn rotate(self: &mut Circle<T>, num_rot: i64) {
+            if num_rot >= 0 {
+                for _ in 0..num_rot.abs() {
+                    self.rotate_clockwise();
+                };
+            } else {
+                for _ in 0..num_rot.abs() {
+                    self.rotate_counterclockwise();
+                };
+            };
+    }
+
+
+    fn print(&mut self) -> () {
+        for elem in self.left.iter() {
+            print!("{:?} ", elem);
+        };
+
+        match &self.current {
+            Some(elem) => print!("({:?}) ", elem),
+            None => print!("")
+        };
+
+        for elem in self.right.iter() {
+            print!("{:?} ", elem);
+
+        };
+        print!("\n");
     }
 }
 
@@ -42,6 +88,17 @@ fn main() -> Result<(), std::io::Error> {
     let num_players = args[1].parse::<usize>().unwrap();
     let num_marbles = args[2].parse::<usize>().unwrap();
 
+    let mut game = Circle::new();
+    game.insert(1);
+    game.insert(2);
+    game.insert(3);
+    game.insert(4);
+    game.print();
+    game.rotate(1);
+    game.print();
+    game.rotate(3);
+    game.print();
+    game.rotate(-8);
     println!("Part 1: highest score = {}", part_1(num_players, num_marbles));
 
     Ok(())
