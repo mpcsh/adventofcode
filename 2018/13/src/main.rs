331e13b919bd471fa3d57ca56b988a8a54e55db5
Mark Cohen <m@mpc.sh>
2019-01-12 11:55:27 -0600

    Track & cart scaffolding done
    Co-authored-by: Sophie Welber <sjwelber@gmail.com>

--- 2018/13/src/main.rs
+++ 2018/13/src/main.rs
@@ -1,5 +1,63 @@
 use std::env;
 use std::fs;
+use std::fmt;
+
+#[derive(PartialEq, Eq)]
+enum Cart {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Cart {
+    fn value(&self) -> char {
+        match *self {
+            Cart::Up => '^',
+            Cart::Down => 'v',
+            Cart::Left => '<',
+            Cart::Right => '>',
+        }
+    }
+}
+
+impl fmt::Display for Cart {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value())
+    }
+}
+
+#[derive(PartialEq, Eq)]
+enum Track {
+    Horizontal,
+    Vertical,
+    Intersection,
+    LeftCurve,
+    RightCurve,
+}
+
+impl Track {
+    fn value(&self) -> char {
+        match *self {
+            Track::Horizontal => '-',
+            Track::Vertical => '|',
+            Track::Intersection => '+',
+            Track::LeftCurve => '\\',
+            Track::RightCurve => '/'
+        }
+    }
+}
+
+impl fmt::Display for Track {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value())
+    }
+}
+
+
+// struct GridPoint {
+
+
 
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
