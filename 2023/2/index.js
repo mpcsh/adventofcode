f17fd98dd9dbfb484622042be325ac6be701ad93
Mark Cohen <git@mail.mpc.sh>
2024-01-16 22:01:22 -0800

    Completed day 2 part 2

--- 2023/2/index.js
+++ 2023/2/index.js
@@ -2,27 +2,53 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const INPUT = (0, fs_1.readFileSync)('./input.txt').toString().split('\n');
-const MAXIMA = {
-    "red": 12,
-    "green": 13,
-    "blue": 14,
-};
-function compute() {
+const EXAMPLE = (0, fs_1.readFileSync)('./example1.txt').toString().split('\n');
+function parseSubSubGame(subSubGameStr) {
+    const { quantityStr, color } = subSubGameStr
+        .match(/^(?<quantityStr>\d+) (?<color>\w+)$/)
+        ?.groups ?? {};
+    // console.log(`Game ${id}: ${quantity} ${color}`);
+    return { color, quantity: Number(quantityStr) };
+}
+function parseSubGames(subGamesStr) {
+    let subGames = [];
+    for (const subGameStr of subGamesStr.split('; ')) {
+        let subGame = [];
+        for (const subSubGameStr of subGameStr.split(', ')) {
+            subGame.push(parseSubSubGame(subSubGameStr));
+        }
+        subGames.push(subGame);
+    }
+    return subGames;
+}
+function parseGame(gameStr) {
+    const { idStr, subGamesStr } = gameStr
+        .match(/^Game (?<idStr>\d+): (?<subGamesStr>.*)$/)
+        ?.groups ?? {};
+    const game = {
+        id: Number(idStr),
+        subGames: parseSubGames(subGamesStr),
+    };
+    // console.log(`Game ${game.id}: ${game.subGames}`);
+    return game;
+}
+function parseGames(gameStrs) {
+    return gameStrs.map(parseGame);
+}
+function part1(input) {
+    const MAXIMA = {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+    const games = parseGames(input);
     let sum = 0;
-    for (const line of INPUT) {
-        const { id, game } = line
-            .match(/^Game (?<id>\d+): (?<game>.*)$/)
-            ?.groups ?? {};
+    for (const { id, subGames } of games) {
         let isPossible = true;
-        for (const subgame of game.split('; ')) {
-            for (const subsubgame of subgame.split(', ')) {
-                const { quantity, color } = subsubgame
-                    .match(/^(?<quantity>\d+) (?<color>\w+)$/)
-                    ?.groups ?? {};
-                // console.log(`Game ${id}: ${quantity} ${color}`);
-                if (Number(quantity) > MAXIMA[color]) {
+        for (const subGame of subGames) {
+            for (const { color, quantity } of subGame) {
+                if (quantity > MAXIMA[color]) {
                     isPossible = false;
-                    // console.log(`Game ${id} impossible due to ${quantity} ${color}`);
                 }
             }
         }
@@ -32,5 +58,26 @@ function compute() {
     }
     return sum;
 }
-const part1 = compute();
-console.log(`Part 1: ${part1}`);
+function power(game) {
+    let maxima = {
+        red: 0,
+        green: 0,
+        blue: 0,
+    };
+    for (const subGame of game.subGames) {
+        for (const { color, quantity } of subGame) {
+            if (quantity > maxima[color]) {
+                maxima[color] = quantity;
+            }
+        }
+    }
+    return maxima.red * maxima.green * maxima.blue;
+}
+function part2(input) {
+    const games = parseGames(input);
+    return games.reduce((sum, game) => sum + power(game), 0);
+}
+const part1Result = part1(INPUT);
+console.log(`Part 1: ${part1Result}`);
+const part2Result = part2(INPUT);
+console.log(`Part 2: ${part2Result}`);
--- 2023/2/index.ts
+++ 2023/2/index.ts
@@ -1,35 +1,79 @@
 import { readFileSync } from "fs";
 
 const INPUT = readFileSync('./input.txt').toString().split('\n');
+const EXAMPLE = readFileSync('./example1.txt').toString().split('\n');
 
-const MAXIMA: Record<string, number> = {
-  "red": 12,
-  "green": 13,
-  "blue": 14,
-};
+type Game = {
+  id: number,
+  subGames: SubGame[],
+}
 
-function compute(): number {
+type SubGame = SubSubGame[];
+
+type SubSubGame = {
+  color: string,
+  quantity: number,
+}
+
+function parseSubSubGame(subSubGameStr: string): SubSubGame {
+  const { quantityStr, color } = subSubGameStr
+    .match(/^(?<quantityStr>\d+) (?<color>\w+)$/)
+    ?.groups ?? {};
+  // console.log(`Game ${id}: ${quantity} ${color}`);
+  return { color, quantity: Number(quantityStr) };
+}
+
+
+function parseSubGames(subGamesStr: string): SubGame[] {
+  let subGames = [];
+  for (const subGameStr of subGamesStr.split('; ')) {
+    let subGame = [];
+    for (const subSubGameStr of subGameStr.split(', ')) {
+      subGame.push(parseSubSubGame(subSubGameStr));
+    }
+    subGames.push(subGame);
+  }
+  return subGames;
+}
+
+function parseGame(gameStr: string): Game {
+  const { idStr, subGamesStr } = gameStr
+    .match(/^Game (?<idStr>\d+): (?<subGamesStr>.*)$/)
+    ?.groups ?? {};
+
+  const game = {
+    id: Number(idStr),
+    subGames: parseSubGames(subGamesStr),
+  };
+
+  // console.log(`Game ${game.id}: ${game.subGames}`);
+
+  return game;
+}
+
+function parseGames(gameStrs: string[]): Game[] {
+  return gameStrs.map(parseGame);
+}
+
+function part1(input: string[]): number {
+  const MAXIMA: Record<string, number> = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+
+  const games = parseGames(input);
+
   let sum = 0;
-
-  for (const line of INPUT) {
-    const { id, game } = line
-      .match(/^Game (?<id>\d+): (?<game>.*)$/)
-      ?.groups ?? {};
+  for (const { id, subGames } of games) {
     let isPossible = true;
-    for (const subgame of game.split('; ')) {
-      for (const subsubgame of subgame.split(', ')) {
-        const { quantity, color } = subsubgame
-          .match(/^(?<quantity>\d+) (?<color>\w+)$/)
-          ?.groups ?? {};
-        // console.log(`Game ${id}: ${quantity} ${color}`);
-
-        if (Number(quantity) > MAXIMA[color]) {
+    for (const subGame of subGames) {
+      for (const { color, quantity } of subGame) {
+        if (quantity > MAXIMA[color]) {
           isPossible = false;
-          // console.log(`Game ${id} impossible due to ${quantity} ${color}`);
         }
       }
     }
-
     if (isPossible) {
       sum += Number(id);
     }
@@ -38,5 +82,31 @@ function compute(): number {
   return sum;
 }
 
-const part1 = compute();
-console.log(`Part 1: ${part1}`);
+function power(game: Game): number {
+  let maxima: Record<string, number> = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  };
+
+  for (const subGame of game.subGames) {
+    for (const { color, quantity } of subGame) {
+      if (quantity > maxima[color]) {
+        maxima[color] = quantity;
+      }
+    }
+  }
+
+  return maxima.red * maxima.green * maxima.blue;
+}
+
+function part2(input: string[]): number {
+  const games = parseGames(input);
+  return games.reduce((sum, game) => sum + power(game), 0);
+}
+
+
+const part1Result = part1(INPUT);
+console.log(`Part 1: ${part1Result}`);
+const part2Result = part2(INPUT);
+console.log(`Part 2: ${part2Result}`);
